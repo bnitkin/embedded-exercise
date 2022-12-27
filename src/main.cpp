24811792045d4643aae175a40a71d7b457959a30
Ben Nitkin <ben@nitkin.net>
2022-12-26 21:38:03 -0500

    One message parsing successfully! Still need to write in functionality for retrieving messages, but it's getting there.

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,8 @@
 
 int main() {
    printf("Hello, world!\n");
-   //onDataFromChip(uint8_t* data, int size) {
+   StreamDecoder decoder;
+   //                ID    ID2   dev   seq   type  load....
+   uint8_t data[] = {0x12, 0x24, 0x0F, 0x99, 0x01, 0xDE, 0xAD, 0x0F, 0x01, 0x01, 0x04, 0x7f};
+   decoder.onDataFromChip(data, sizeof(data));
 }
